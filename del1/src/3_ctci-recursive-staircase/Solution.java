a886135216781a0609550d792c7441be2a316eca
Octopod <denise.nordlof@gmail.com>
2018-04-22 12:03:37 +0200

    new solution passed all tests

--- del1/src/3_ctci-recursive-staircase/Solution.java
+++ del1/src/3_ctci-recursive-staircase/Solution.java
@@ -1,39 +1,42 @@
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+package se.denisen;
 
-public class Solution {
-    static int recursiveStairCounter(int steps){
-        int returnValue = 0;
+import java.util.Scanner;
 
-        if (steps < 0)
+class Solution {
+    private static int stairCounter(int steps){
+        int[] returnValueCounter = new int[steps];
+
+        if (steps < 1)
             return 0;
-        if (steps == 0)
+        if (steps == 1)
             return 1;
+        if (steps == 2)
+            return 2;
+        if (steps == 3)
+            return 4;
 
-        returnValue = recursiveStairCounter(steps-1);
-        returnValue += recursiveStairCounter(steps-2);
-        returnValue += recursiveStairCounter(steps-3);
-        return returnValue;
+        returnValueCounter[0] = 1;
+        returnValueCounter[1] = 2;
+        returnValueCounter[2] = 4;
 
-
+        int i = 3;
+        while (i < steps) {
+            returnValueCounter[i] = returnValueCounter[i-3] + returnValueCounter[i-2] + returnValueCounter[i-1];
+            i++;
+        }
+        return returnValueCounter[steps-1];
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int nrOfStairs = in.nextInt();
-        int nrOfWays=0;
         int[] listOfStairs = new int[nrOfStairs];
         for(int a0 = 0; a0 < nrOfStairs; a0++){
             int n = in.nextInt();
-            listOfStairs[a0] = recursiveStairCounter(n);
-            //nrOfWays += recursiveStairCounter(n);
+            listOfStairs[a0] = stairCounter(n);
         }
         for (int stair:listOfStairs) {
             System.out.println(stair);
         }
-
     }
 }
