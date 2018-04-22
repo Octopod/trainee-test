334c6267086db6ac3feea712b690de4d31b7daaa
Octopod <denise.nordlof@gmail.com>
2018-04-22 12:19:35 +0200

    smaller changes to achieve some generification

--- del1/src/2_countingsort4/Solution.java
+++ del1/src/2_countingsort4/Solution.java
@@ -1,12 +1,8 @@
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
-public class Solution {
+class Solution {
 
-    static Map createMap(){
+    private static Map<Integer, ArrayList<String>> createMap(){
         Map<Integer, ArrayList<String>> mapOfInput = new HashMap<>(100);
 
         Scanner in = new Scanner(System.in);
@@ -20,24 +16,23 @@ public class Solution {
                 mapOfInput.put(x, new ArrayList<>());
                 list = mapOfInput.get(x);
             }
-            if (i<(nrOfPairs/2)){
+            if (i < (nrOfPairs / 2)) {
                 list.add("-");
-            }else
-                list.add(s);
+            } else list.add(s);
         }
         in.close();
         return mapOfInput;
     }
 
     public static void main(String[] args) {
-        Map map = createMap();
-        Iterator entries = map.entrySet().iterator();
+        Map<Integer, ArrayList<String>> map = createMap();
+        Iterator<Map.Entry<Integer, ArrayList<String>>> entries = map.entrySet().iterator();
         StringBuilder resultString = new StringBuilder();
         while (entries.hasNext()) {
-            Map.Entry entry = (Map.Entry) entries.next();
-            ArrayList<String> value = (ArrayList<String>)entry.getValue();
+            Map.Entry<Integer, ArrayList<String>> entry = entries.next();
+            ArrayList<String> value = entry.getValue();
             for (String listEntry: value) {
-                resultString.append(listEntry + " ");
+                resultString.append(listEntry).append(" ");
             }
         }
         String end = resultString.toString().trim();
